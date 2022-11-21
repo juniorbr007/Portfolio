02cc949ed616575e51bef5545397904881e1c507
João Batista <junior_jack007@hotmail.com>
2022-11-21 04:42:14 -0300

    Animations

--- portfolio/src/app/components/home/home.component.ts
+++ portfolio/src/app/components/home/home.component.ts
@@ -12,6 +12,21 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        console.log(entry)
+
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('shlow');
+        }
+      })
+    });
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) =>
+      observer.observe(el));
   }
 
 }
